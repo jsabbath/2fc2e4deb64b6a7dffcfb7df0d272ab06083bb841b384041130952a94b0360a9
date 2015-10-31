7c336041413bdf4254ce586d0aa809da49f8b559
jsabbath <zahoz.moopa@gmail.com>
2015-10-31 14:50:49 -0500

    login
    login inicial

--- index.php
+++ index.php
@@ -1,9 +1,22 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Principal</title>
 </head>
 <body>
-
+	<form>
+		<div>
+			<input type="text" placeholder="Usuario">
+		</div>
+		<div>
+			<input type="password" placeholder="contraseña">
+		</div>
+		<div>
+			<input type="button" text="Ingresar">
+		</div>
+		<div>
+			<input type="text" name="" class="" id="">
+		</div>
+	</form>
 </body>
 </html>
