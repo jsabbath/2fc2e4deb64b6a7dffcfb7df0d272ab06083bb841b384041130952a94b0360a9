5b55f93f7c514ddb3662b27d60c180513202eb0f
NtkO Daigo <degitalpet@gmail.com>
2015-11-01 05:07:23 +0900

    Test login

---
+++ login.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @author ME
+ * @copyright 2015
+ */
+    <!DOCTYPE HTML>
+    <html>
+    <head>
+    	<meta http-equiv="content-type" content="text/html" />
+    	<meta name="ME" content="Modulo de Vacaciones - Login" />
+    
+    	<title>Login</title>
+    </head>
+    
+    <body>
+    
+    
+    
+    </body>
+    </html>
+
+
+?>
