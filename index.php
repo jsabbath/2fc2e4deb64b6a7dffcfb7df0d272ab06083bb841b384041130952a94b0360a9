03008447c694816d3f1282a83b30ddfdfb81f481
jsabbath <zahoz.moopa@gmail.com>
2015-11-01 20:56:31 -0500

    loginCompleto
    se requiere de la creacion de la DB para poder avanzar

--- index.php
+++ index.php
@@ -1,22 +1,43 @@
-<!DOCTYPE html>
 <html>
+<meta charset="utf-8">
+<link href="css/style.css" rel="stylesheet" />
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
 <head>
 	<title>Principal</title>
 </head>
+
 <body>
-	<form>
-		<div>
-			<input type="text" placeholder="Usuario">
+	<div class="navbar">
+		<img src="img/logo.png">
+	</div>
+
+	<div class="contenedor">
+		<div class="formulario_login">
+			<div class="title1">
+				AUTENTICACIÓN
+			</div>
+			<div class="divider"></div>
+			<img src="img/perfil.png" class="img-flat">
+				<form action="login.php" method="POST" data-wow-offset="10" data-wow-delay="0.2s">
+					<div>
+						<input class= "textbox" name="user" type = "text" placeholder = "Ingrese usuario" required>
+					</div>
+					<div>
+						<input class= "textbox" name="pass" type = "password" placeholder = "Ingrese contraseña" required>
+					</div>
+					<div>
+						<input  class="button_BLUEJEANS" type = "submit" value="Ingresar" href="modulos/panelControl.php">
+						<br>
+					</div>
+					<!--div class="alert alert-danger">
+						Error ! Change few things.
+					</div--> 
+											
+				</form>
 		</div>
-		<div>
-			<input type="password" placeholder="contraseña">
-		</div>
-		<div>
-			<input type="button" text="Ingresar">
-		</div>
-		<div>
-			<input type="text" name="" class="" id="">
-		</div>
-	</form>
+	</div>
+
 </body>
 </html>
