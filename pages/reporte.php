fc521bd935a05d234bddeb1c891d17989534c1a2
jsabbath <zahoz.moopa@gmail.com>
2015-11-04 12:11:16 -0500

    reporte vista
    diseño html de reporte

---
+++ pages/reporte.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Control Vacacional</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="../bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- MetisMenu CSS -->
+    <link href="../bower_components/metisMenu/dist/metisMenu.min.css" rel="stylesheet">
+
+    <!-- DataTables CSS -->
+    <link href="../bower_components/datatables-plugins/integration/bootstrap/3/dataTables.bootstrap.css" rel="stylesheet">
+
+    <!-- DataTables Responsive CSS -->
+    <link href="../bower_components/datatables-responsive/css/dataTables.responsive.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="../dist/css/sb-admin-2.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="../bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body >
+	<!--detalles trabajador-->
+	<div class="contenedor center-block">
+		<div class="details">
+		<div class="titledoc">
+			<h1>Record de Vacaciones</h1>
+		</div>
+		<ul class="lista_datos">
+			<li>
+				<strong>Area: </strong>TI
+			</li>
+			<li>
+				<strong>Corte Superior: </strong>Pedro calderon de la barca
+			</li>
+			<li>
+				<strong>Apellidos y Nombres: </strong>Pedro calderon de la barca
+			</li>
+			<li>
+				<strong>Escalofon: </strong>33693
+			</li>
+			<li>
+				<strong>Fecha Ingreso: </strong>01/11/2009
+			</li>
+			<li>
+				<strong>Fecha Ingreso: </strong>01/11/2009
+			</li>
+			<li>
+				<strong>Fecha Cese: </strong>03/03/2015
+			</li>
+			<li>
+				<strong>Fecha Cese: </strong>03/03/2015
+			</li>
+			<li>
+				<strong>Regimen Laboral: </strong>728ª - Plazo indeterminado
+			</li>
+		</ul>
+	<div class="row">
+
+	<!--tabla detalles-->
+	<div class="col-sm-12 table-responsive">
+		<table aria-describedby="dataTables-example_info" role="grid" class="table table-striped table-bordered dataTable no-footer" id="dataTables-example">
+	        <thead>
+	            <tr role="row">
+	            <th style="width: 50px;" colspan="1" rowspan="1" aria-controls="dataTables-example" tabindex="0">
+	            Periodo
+	            </th>
+	            <th aria-sort="ascending" style="width: 90px;" colspan="1" rowspan="1" aria-controls="dataTables-example" tabindex="0" >
+	            Vacaciones Gozadas</th>
+	            <th aria-label="Fecha Ingreso: activate to sort column ascending" style="width: 72px;" colspan="1" rowspan="1" aria-controls="dataTables-example" tabindex="0">
+	            Cantidad Dias</th>
+	            <th aria-label="Fecha Cese: activate to sort column ascending" style="width: 65px;" colspan="1" rowspan="1" aria-controls="dataTables-example" tabindex="0">
+	            Estado</th>
+	        </thead>
+	        <tbody>
+	            
+	        <tr role="row" class="gradeC odd">
+	                <td class="">Han solo Verger piñas</td>
+	                <td class="sorting_1">Almacen / 1017</td>
+	                <td>09/08/2003</td>
+	                <td class="center">23/08/2002</td>
+	            </tr>
+	            <tr role="row" class="gradeA even">
+	                <td class="">Alan Garcia Solazo</td>
+	                <td class="sorting_1">Almacen / 1017</td>
+	                <td>15/08/1990</td>
+	                <td class="center">23/08/1992</td>
+	            </tr>
+	            <tr role="row" class="gradeA odd">
+	                <td class="">Andrea Lizeth Atuhuaman jurad</td>
+	                <td class="sorting_1">Almacen / 728</td>
+	                <td>09/09/2009</td>
+	                <td class="center">10/10/2015</td>
+	            </tr>
+	            <tr role="row" class="gradeC even">
+	                <td class="">Victor Luck Uzumaki Mazgo</td>
+	                <td class="sorting_1">Logistica / 1017</td>
+	                <td>13/05/2000</td>
+	                <td class="center">12/08/2002</td>
+	            <tr role="row" class="gradeA even">
+	                <td class="">Celene Margit levano soriano</td>
+	                <td class="sorting_1">Marketing / 728</td>
+	                <td>12/03/1994</td>
+	                <td class="center">23/08/2002</td>
+	            </tr>
+	            </tbody>
+	    </table>
+        </div>
+	</div>
+	</div>
+</body>
+
+<style type="text/css">
+	body{
+		background-color: gray;
+	}
+	.contenedor{
+		padding-bottom: 20px;
+		width: 920px;
+		background-color: white;
+		/*border-color: black;*/
+		position: none;
+		margin-bottom: 20p;
+		margin-top: 50px;
+		height: 750px;
+	}
+	.details{
+
+	}
+	.titledoc{
+		text-align: center;
+		font-size: 14px;
+		font-family: Roboto;
+		padding-bottom: 20px;
+		margin-bottom: 20px;
+		padding-top: 30px;
+	}
+	.lista_datos{
+		font-size: 15px;
+		font-family: Roboto;
+		padding-left: 15%;
+		list-style: none;
+	}
+	li{
+		text-decoration: none;
+		margin-bottom: 5px;
+	}
+	table{
+		width: 500px;
+	}
+</style>
+
+</html>
