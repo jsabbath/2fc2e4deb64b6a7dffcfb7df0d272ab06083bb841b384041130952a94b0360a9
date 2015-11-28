9a6a4f7963981bdfe3b4b00fff8cd89294a99581
jsabbath <zahoz.moopa@gmail.com>
2015-11-27 23:01:34 -0500

    newNEW

--- pages/include/regCRUArea.php
+++ pages/include/regCRUArea.php
@@ -4,31 +4,18 @@
 
 	$vcnombre = strip_tags($_POST['vcnombre']); //varibless
 
-	$query = @mysql_query('SELECT * FROM tbarea WHERE vcnombre="'.mysql_real_escape_string($vcnombre).'"');
+	$query = @mysql_query('SELECT * FROM tbarea WHERE vcnombreArea="'.mysql_real_escape_string($vcnombre).'"');
 		if ($existe = @mysql_fetch_object($query))
 			{
 				echo "El registro ya existe";
 			}else{
-				$meter = @mysql_query('INSERT INTO tbarea (cid, vcnombre) values ("","'.mysql_real_escape_string($vcnombre).'");');
+				$meter = @mysql_query('INSERT INTO tbarea (cid, vcnombreArea) values ("","'.mysql_real_escape_string($vcnombre).'");');
 				if($meter)
 					{
 						
-						echo "<script type='text/javascript'>
-								document.write('<! - Pequeño modal -> 
-												<button  type = 'button'  class = 'btn  btn-primaria'-toggle de datos = 'modal' diana de datos='.BS-ejemplo-modal-sm'> Pequeño modal </ botón>
-												<div  class = 'fade modal modal-sm'> 
-												    <div  class = 'modal-content'>
-												      registro Exitoso
-												    </ div> 
-												  </ div> 
-												</ div>');
-							  </script>";
 						header ("Location: .././CRUArea.php");//echo "<div>registro existoso </div>";
 						//$cid = $cid ++;
 					}else 
-						echo "<script type=¡text/javascript'>
-								$('.message').removeClass('')
-							  </script>";
-						//echo "<div>registro error </div>";
+						echo "<div>registro error </div>";
 				}
 ?>
